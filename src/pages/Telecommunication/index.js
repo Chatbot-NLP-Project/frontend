f933022f3a44bc95031b5174258315dddc94c39f
Chalindu Malshika <chalindu.18@cse.mrt.ac.lk>
2021-09-12 00:44:41 +0530

    Error fixed

--- src/pages/Telecommunication/index.js
+++ src/pages/Telecommunication/index.js
@@ -1,12 +1,13 @@
 import BottomFooter from "../../components/Footer/BottomFooter";
 import Footer from "../../components/Footer/Footer";
+// import EducationMain from "../../components/Telecom/EducationMain";
 import ImageSlider from "../../components/Slider/ImageSlider";
 import {SliderData} from "../../components/Slider/SliderData";
 
 function Education() {
     return(
         <>
-         
+            {/* <EducationMain /> */}
             <ImageSlider slides = {SliderData} />
             <Footer />
             <BottomFooter botName="XYRON" />
