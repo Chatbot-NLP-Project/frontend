9bb18bf6aff08f7ecc22be49672922642b9346c1
Chalindu Malshika <chalindu.18@cse.mrt.ac.lk>
2021-10-22 19:35:46 +0530

    Unit testing

--- myFiles.js
+++ myFiles.js
@@ -60,30 +60,39 @@
 //react-scripts test
 
 
-return (
-    <ChakraProvider>
-      <Router>
-      <Navbar Auth={auth} />
-        <Switch >
-            {/* Common Routes */}
-            <Route path="/telecommunication" exact component={Telecommunication} exact/>
-            <Route path="/healthcare" exact component={Healthcare} exact/>
-            <Route path="/transport" exact component={Transport} exact/>
-            <Route path="/chatbothc" exact component={ChatbotHC} />
-            <Route path="/chatbotpt" exact component={ChatbotPT} />
-            <Route path="/chatbottc" exact component={ChatbotTC} />
-            {/* <Route path="/profile/:id" exact component={Profile}/> */}
-            <Route path="/profile" exact component={Profile} />
-            <Route path="/rating" exact component={Rating} />
-            <Route path="/admin" exact component={Admin} />
-            <Route path="/domain" component={DomaineSelection} />
-            <Route path="/signUp" exact component={SignUp} />
-            <Route path="/rating" exact component={Rating} />
-            <Route path="/admin" exact component={Admin} />
-            <Route path="/signin" exact component={ () => {return <SignIn setSignInClicked={setSignInClicked}/>}} />
-          {/* <Route path="/domainselection" exact component={DomainSelection} exact/> */}
-          <Route path="/"  component={LandingPage} />
-          </Switch>
-        </Router>
-      </ChakraProvider>
-    );
+// return (
+//     <ChakraProvider>
+//       <Router>
+//       <Navbar Auth={auth} />
+//         <Switch >
+//             {/* Common Routes */}
+//             <Route path="/telecommunication" exact component={Telecommunication} exact/>
+//             <Route path="/healthcare" exact component={Healthcare} exact/>
+//             <Route path="/transport" exact component={Transport} exact/>
+//             <Route path="/chatbothc" exact component={ChatbotHC} />
+//             <Route path="/chatbotpt" exact component={ChatbotPT} />
+//             <Route path="/chatbottc" exact component={ChatbotTC} />
+//             {/* <Route path="/profile/:id" exact component={Profile}/> */}
+//             <Route path="/profile" exact component={Profile} />
+//             <Route path="/rating" exact component={Rating} />
+//             <Route path="/admin" exact component={Admin} />
+//             <Route path="/domain" component={DomaineSelection} />
+//             <Route path="/signUp" exact component={SignUp} />
+//             <Route path="/rating" exact component={Rating} />
+//             <Route path="/admin" exact component={Admin} />
+//             <Route path="/signin" exact component={ () => {return <SignIn setSignInClicked={setSignInClicked}/>}} />
+//           {/* <Route path="/domainselection" exact component={DomainSelection} exact/> */}
+//           <Route path="/"  component={LandingPage} />
+//           </Switch>
+//         </Router>
+//       </ChakraProvider>
+//     );
+
+
+// npm run test:CI
+
+//To get the stats of you test coverage, you can run below command which will generate the coverage report inside “coverage/lcov-report/index.html” of project root folder
+// npm run test -- --coverage
+//            or
+// npm run test:CI -- --coverage
+
