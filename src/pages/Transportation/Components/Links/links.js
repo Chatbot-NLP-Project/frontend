e2be39e30ce2b33a05b6263fa84e9225ff6a0014
GeethKavinda <kavindag.18@cse.mrt.ac.lk>
2021-11-03 16:51:43 +0530

    reservation links added

---
+++ src/pages/Transportation/Components/Links/links.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+import {
+  Box,
+  Heading,
+  StackItem,
+  Stack,
+  Flex,
+  Link,
+  Button,
+} from "@chakra-ui/react";
+
+const Links = (props) => {
+  const renderMethod = () => {
+    return props.links.map((lnk) => {
+      // function selectDate() {
+      //   props.actionProvider.selectDoctor(props.methods.indexOf(method));
+      //   console.log(props);
+      // }
+      return (
+        <StackItem key={lnk.lnkID}>
+          <Box
+            boxShadow="0 0 2px 2px #ceddeb"
+            bg="#f0f4f5"
+            width="40%"
+            borderWidth="1px"
+            borderRadius="5px"
+            padding="10px"
+            // mr={40}
+            mb={1}
+          >
+            <Flex>
+              <Heading as="h3" size="sm" color="#0a1a30" ml={3}>
+                {lnk.lnkName}
+              </Heading>
+            </Flex>
+            <Heading as="h4" size="xs" color="#186273" ml={3}>
+              Visit: <Link color="teal.500" href={lnk.lnkLink}>
+                {lnk.lnkSite}
+                </Link>
+            </Heading>
+            {/* <Link to="/home">
+              <Button
+                width="100%"
+                colorScheme="blue"
+                color="#00004d"
+                variant="outline"
+                lineHeight="1"
+                height="30px"
+                padding={1}
+                size="md"
+                // ml={3}
+                mr={3}
+                onClick={selectDate}
+              >
+                Channel
+              </Button>
+            </Link> */}
+          </Box>  
+        </StackItem>
+      );
+    });
+  };
+
+  return <Stack ml="10%">{renderMethod()}</Stack>;
+};
+
+export default Links;
