3e80e9eacb190cdf588b5a91bee5cabc25016c32
Sandaruwan Kumarasinghe <62076661+Sandaruwan1229@users.noreply.github.com>
2021-11-05 20:01:12 +0530

    Update messageParser.js

--- src/pages/Healthcare/Chatbot/messageParser.js
+++ src/pages/Healthcare/Chatbot/messageParser.js
@@ -13,7 +13,7 @@ class MessageParser {
 
     if (
       this.state.currentState[siz] == "predict" &&
-      this.state.sympthoms.length < 4
+      this.state.sympthoms.length < 3
     ) {
       this.actionProvider.addDisease(message);
       if (this.state.sympthoms.length == 3) {
